722eae9c862e7c838cbf9b8365ed26c5cbd74d09
AhmedOsama099 <ahmedeng099@gmail.com>
2024-08-03 19:42:26 +0300

    Add brand color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        brandColor: "#9C7C35",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
